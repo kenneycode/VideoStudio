e53ddc22d0282b2117b2ccc31fc432e97002e28c
kenneycode <kenneycode@qq.com>
2019-09-09 23:05:46 +0800

    commit

--- app/src/main/java/io/github/kenneycode/videostudio/demo/MainActivity.kt
+++ app/src/main/java/io/github/kenneycode/videostudio/demo/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.github.kenneycode.videostudio
+package io.github.kenneycode.videostudio.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ libvideostudio/src/main/java/io/github/kenneycode/videostudio/VideoDecoder.java
@@ -0,0 +1,4 @@
+package io.github.kenneycode.videostudio;
+
+public class VideoDecoder {
+}
